ddcb9e3581dc9e5cbc4f0b435593453eefd759ae
jantorno12 <danieljantorno@hotmail.com>
2021-10-23 15:24:54 -0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.sendall(send_length)
+    client.sendall(message)
+    print(client.recv(2048).decode(FORMAT))
+
+dict = {'Jantorno': 'Forte', 'Bernardo': 'Fraco'}
+send(str(dict))
+input()
+send("Hello Everyone!")
+send("Hello Tim!")
+
+send(DISCONNECT_MESSAGE)
