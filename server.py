3c3a2389e7de54dfcc7b2d3068ef16285ec0959d
bernardo <betepedino@gmail.com>
2021-10-30 16:00:45 -0300

    concluindo o spin

--- server.py
+++ server.py
@@ -9,39 +9,106 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 min_bet = 5
-saldo = {'cliente' : 1000, 'servidor': 1000}
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def lose_bet(status):
+    aposta = status['bet']
+    valor_cliente = status['cliente']
+    status['cliente'] = valor_cliente - aposta
+    status['resultado'] = 'Perdeu'
+    return status
+
+def win_bet(status, times):
+    aposta = status['bet']
+    valor_cliente = status['cliente']
+    status['cliente'] = valor_cliente + aposta*(times-1)
+    status['resultado'] = 'Venceu'
+    return status
+
 def type_bet(dict, tipo):
     resp = random.randint(-1, 36)
+    red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
+    black = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
     if(tipo < 37):
         print(f'Foi apostado no numero {tipo}')
+        if (tipo ==  resp):
+            return win_bet(dict, 36)
+        else:
+            return lose_bet(dict)
     if(tipo == 37):
         print(f'Foi apostado nos numeros pares')
+        if (resp % 2 == 0):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 38):
         print(f'Foi apostado nos numeros impares') 
+        if (resp % 2 == 1):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 39):
         print(f'Foi apostado nos numeros vermelhos') 
+        if (resp in  red):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 40):
         print(f'Foi apostado nos numeros pretos') 
+        if (resp in  black):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 41):
-        print(f'Foi apostado na primeira coluna de numeros') 
+        print(f'Foi apostado na primeira coluna de numeros')
+        if (resp % 3 == 1):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict) 
     if(tipo == 42):
         print(f'Foi apostado na segunda coluna de numeros') 
+        if (resp % 3 == 2):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict) 
     if(tipo == 43):
         print(f'Foi apostado na terceira coluna de numeros') 
+        if (resp % 3 == 0):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict) 
     if(tipo == 44):
-        print(f'Foi apostado nos numeros de 1 a 18') 
+        print(f'Foi apostado nos numeros de 1 a 18')
+        if (resp < 19):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 45):
         print(f'Foi apostado nos numeros de 19 a 36')
+        if (resp > 19):
+            return win_bet(dict, 2)
+        else:
+            return lose_bet(dict)
     if(tipo == 46):
         print(f'Foi apostado nos numeros de 1 a 12')
+        if (resp < 13):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict)
     if(tipo == 47):
         print(f'Foi apostado nos numeros de 13 a 24')
+        if (12 < resp < 25):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict)
     if(tipo == 48):
         print(f'Foi apostado nos numeros de 25 a 36')
+        if (24 < resp < 37):
+            return win_bet(dict, 3)
+        else:
+            return lose_bet(dict)
         
 def check_bet(dict, bet):
     print(f'Valor enviado para aposta: {int(bet)}')
@@ -85,15 +152,14 @@ def play_roulette(conn, addr, status):
             if excludespace(action_type) == 'BET':
                 msg = trata_msg(conn)
                 status['bet'], ans = check_bet(status ,int(msg))
-                aux_dict = status
-                aux_dict['resposta'] = ans
-                sendpackage(conn, str(aux_dict))
+                status['resposta'] = ans
+                sendpackage(conn, str(status))
 
             if excludespace(action_type) == 'SPIN':
                 msg = trata_msg(conn)
                 status = type_bet(status, int(msg))
+                sendpackage(conn, str(status))
                 
-
             if excludespace(action_type) == DISCONNECT_MESSAGE:
                 connected = False
 
@@ -106,7 +172,7 @@ def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     #Botar multi tread se quiser
-    saldo = {'cliente' : 1000, 'mesa': 1000}
+    saldo = {'cliente' : 1000}
     conn, addr = server.accept()
     play_roulette(conn,addr, saldo)
 
